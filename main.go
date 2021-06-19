286fd57412da31839a4709d1575f8b0a7bc15103
Daniel <danielmapar@gmail.com>
2021-06-19 13:45:19 -0400

    Working pumps

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 	"time"
 
 	gorillaWs "github.com/gorilla/websocket"
-	"github.com/iris-contrib/middleware/cors"
 	"github.com/joho/godotenv"
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/websocket"
+	"github.com/kataras/neffos"
 	"github.com/kataras/neffos/gorilla"
 	"go.bug.st/serial"
 	"gocv.io/x/gocv"
@@ -22,21 +22,31 @@ import (
 
 // ngrok http -auth="username:password" 8080
 // nohup go run main.go > ngrok.log &
-// nohup ./ngrok -log=stdout http 8080 > ngrok.log &
+// nohup ./ngrok http 8080 > ngrok.log &
 // curl http://localhost:4040/api/tunnels
 // jobs
 
-func validPassword(ctx iris.Context) error {
-	password := os.Getenv("APP_PASSWORD")
-	if ctx.URLParamDefault("password", "Not a password") != password {
+func validPassword(password string) error {
+	correctPassword := os.Getenv("APP_PASSWORD")
+	if password != correctPassword {
 		return errors.New("Invalid token!")
 	}
 	return nil
 }
 
 func authMiddleware(ctx iris.Context) {
-	if err := validPassword(ctx); err != nil {
+	type authHeader struct {
+		Authorization string `header:"Authorization,required"`
+	}
+	var authHeaders authHeader
+	if err := ctx.ReadHeaders(&authHeaders); err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	if err := validPassword(authHeaders.Authorization); err != nil {
 		ctx.StopWithError(iris.StatusBadRequest, err)
+		return
 	}
 	ctx.Next()
 	return
@@ -50,29 +60,35 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	humiditySensorLimitEnv := "APP_HUMIDITY_SENSOR_LIMIT"
-	lightStatusEnv := "APP_LIGHT_STATUS"
-	pumpStatusEnv := "APP_PUMP_STATUS"
+	humiditySensorLimitEnvVar := "APP_HUMIDITY_SENSOR_LIMIT"
+	lightStatusEnvVar := "APP_LIGHT_STATUS"
+	pumpStatusEnvVar := "APP_PUMP_STATUS"
+	enableCORSEnvVar := "APP_ENABLE_CORS"
+	portEnvVar := "APP_PORT"
 
 	var lightRelayStatus byte
-	if os.Getenv(lightStatusEnv) == "on" {
+	if os.Getenv(lightStatusEnvVar) == "on" {
 		lightRelayStatus = '1'
 	} else {
 		lightRelayStatus = '0'
 	}
 
 	var pumpRelayStatus byte
-	if os.Getenv(pumpStatusEnv) == "on" {
+	if os.Getenv(pumpStatusEnvVar) == "on" {
 		pumpRelayStatus = '1'
 	} else {
 		pumpRelayStatus = '0'
 	}
 
-	// For dev use only
-	upgrader := gorilla.Upgrader(gorillaWs.Upgrader{
-		CheckOrigin: func(*http.Request) bool {
-			return true
-		}})
+	var upgrader neffos.Upgrader = nil
+
+	if os.Getenv(enableCORSEnvVar) == "true" {
+		// For dev use only
+		upgrader = gorilla.Upgrader(gorillaWs.Upgrader{
+			CheckOrigin: func(*http.Request) bool {
+				return true
+			}})
+	}
 
 	websocketServer := websocket.New(upgrader, websocket.Events{
 		websocket.OnNativeMessage: func(nsConn *websocket.NSConn, msg websocket.Message) error {
@@ -83,8 +99,10 @@ func main() {
 
 	websocketServer.OnConnect = func(c *websocket.Conn) error {
 		ctx := websocket.GetContext(c)
-		if err := validPassword(ctx); err != nil {
+
+		if err := validPassword(ctx.URLParamDefault("password", "")); err != nil {
 			c.Close()
+			ctx.StopWithError(iris.StatusInternalServerError, err)
 			return err
 		}
 		log.Printf("[%s] Connected to server!", c.ID())
@@ -102,12 +120,19 @@ func main() {
 	sensorChannel := make(chan []byte)
 	defer close(sensorChannel)
 
+	// Initialize sensor data
+	var sensorOptionsBuffer []byte
+	sensorOptionsBuffer = append(sensorOptionsBuffer, lightRelayStatus)
+	sensorOptionsBuffer = append(sensorOptionsBuffer, pumpRelayStatus)
+
 	// Sensors stream
 	go func(channel chan []byte) {
 		mode := &serial.Mode{
 			BaudRate: 9600, // Same as Arduino code,
 		}
 		port, err := serial.Open("/dev/ttyACM0", mode)
+		defer port.Close()
+
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -120,6 +145,14 @@ func main() {
 		var inputJSON []byte
 
 		for {
+
+			select {
+			case outputBuffer = <-channel:
+				fmt.Printf("Changed switch: %v\n", outputBuffer)
+			default:
+				//fmt.Println("no message received")
+			}
+
 			_, outputBufferErr := port.Write(outputBuffer)
 			if outputBufferErr != nil {
 				log.Fatal(outputBufferErr.Error())
@@ -153,11 +186,7 @@ func main() {
 		}
 	}(sensorChannel)
 
-	var outputBuffer []byte
-	outputBuffer = append(outputBuffer, lightRelayStatus)
-	outputBuffer = append(outputBuffer, pumpRelayStatus)
-
-	sensorChannel <- outputBuffer
+	sensorChannel <- sensorOptionsBuffer
 
 	// Video stream
 	go func() {
@@ -165,6 +194,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		defer camera.Close()
 
 		image := gocv.NewMat()
 
@@ -184,42 +214,63 @@ func main() {
 				}
 				websocketServer.Broadcast(nil, message)
 			}
-			time.Sleep(time.Millisecond * time.Duration(50))
+			time.Sleep(time.Second / 2)
 		}
 	}()
 
 	app := iris.New()
-	app.Get("/realtime", websocket.Handler(websocketServer))
 
 	app.HandleDir("/", iris.Dir("./build"))
 
-	// For dev use only
-	crs := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
-	})
-	app.Use(crs)
-	app.AllowMethods(iris.MethodOptions)
+	app.Get("/realtime", websocket.Handler(websocketServer))
 
-	type HumidityBody struct {
-		Value int
+	if os.Getenv(enableCORSEnvVar) == "true" {
+		// Our custom CORS middleware.
+		crs := func(ctx iris.Context) {
+			ctx.Header("Access-Control-Allow-Origin", "http://localhost:3001")
+			ctx.Header("Access-Control-Allow-Headers", "Content-Type,Authorization,Sec-WebSocket-Protocol")
+			ctx.Header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+			ctx.Header("Access-Control-Allow-Credentials", "true")
+
+			if ctx.Method() == iris.MethodOptions {
+				ctx.Header("Access-Control-Methods",
+					"POST, PUT, PATCH, DELETE")
+
+				ctx.Header("Access-Control-Allow-Headers",
+					"Access-Control-Allow-Origin,Content-Type,Authorization,Sec-WebSocket-Protocol")
+
+				ctx.Header("Access-Control-Max-Age",
+					"86400")
+
+				ctx.StatusCode(iris.StatusNoContent)
+				return
+			}
+			ctx.Next()
+		}
+
+		app.UseRouter(crs)
 	}
 
 	app.Post("/humiditySensorLimit", authMiddleware, func(ctx iris.Context) {
+		type HumidityBody struct {
+			Value int
+		}
+
 		humidityBody := HumidityBody{Value: 400}
+
 		err := ctx.ReadJSON(&humidityBody)
 		if err != nil {
 			ctx.StopWithError(iris.StatusBadRequest, err)
 			return
 		}
-		os.Setenv(humiditySensorLimitEnv, strconv.Itoa(humidityBody.Value))
+		os.Setenv(humiditySensorLimitEnvVar, strconv.Itoa(humidityBody.Value))
 		ctx.JSON(iris.Map{
-			"value": os.Getenv(humiditySensorLimitEnv),
+			"value": os.Getenv(humiditySensorLimitEnvVar),
 		})
 	})
 
 	app.Get("/humiditySensorLimit", authMiddleware, func(ctx iris.Context) {
-		if value, ok := os.LookupEnv(humiditySensorLimitEnv); ok {
+		if value, ok := os.LookupEnv(humiditySensorLimitEnvVar); ok {
 			ctx.JSON(iris.Map{
 				"value": value,
 			})
@@ -230,12 +281,13 @@ func main() {
 		})
 	})
 
-	type LightRelayBody struct {
-		Value string
-	}
-
 	app.Post("/lightRelay", authMiddleware, func(ctx iris.Context) {
+		type LightRelayBody struct {
+			Value string
+		}
+
 		lightRelayBody := LightRelayBody{Value: ""}
+
 		err := ctx.ReadJSON(&lightRelayBody)
 		if err != nil {
 			ctx.StopWithError(iris.StatusBadRequest, err)
@@ -246,13 +298,44 @@ func main() {
 		} else {
 			lightRelayStatus = '0'
 		}
-		outputBuffer[0] = lightRelayStatus
-		sensorChannel <- outputBuffer
+		sensorOptionsBuffer[0] = lightRelayStatus
+		sensorChannel <- sensorOptionsBuffer
+
+		// Wait a bit for update to propagate on the Arduino
+		time.Sleep(time.Second * 3)
 
 		ctx.JSON(iris.Map{
 			"value": lightRelayStatus,
 		})
 	})
 
-	app.Run(iris.Addr(":8080"))
+	app.Post("/pumpRelay", authMiddleware, func(ctx iris.Context) {
+		type PumpRelayBody struct {
+			Value string
+		}
+
+		pumpRelayBody := PumpRelayBody{Value: ""}
+
+		err := ctx.ReadJSON(&pumpRelayBody)
+		if err != nil {
+			ctx.StopWithError(iris.StatusBadRequest, err)
+			return
+		}
+		if pumpRelayBody.Value == "on" {
+			pumpRelayStatus = '1'
+		} else {
+			pumpRelayStatus = '0'
+		}
+		sensorOptionsBuffer[1] = pumpRelayStatus
+		sensorChannel <- sensorOptionsBuffer
+
+		// Wait a bit for update to propagate on the Arduino
+		time.Sleep(time.Second * 3)
+
+		ctx.JSON(iris.Map{
+			"value": pumpRelayStatus,
+		})
+	})
+
+	app.Run(iris.Addr(":" + os.Getenv(portEnvVar)))
 }
